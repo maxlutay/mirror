7c55eda79aa69f20263de9eeb004461b1c2244e8
MaximLutay <maksym.lutai@nure.ua>
2018-10-19 02:19:23 +0300

    try env

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,6 +1,21 @@
+
+const http = require("http");
+
+const source = process.env.SITE
+let body;
+
+if(!!source){
+    body = http.get(source);
+}else {
+    body = "<h1> omg, wtf</h1>";
+}
+
+
+
+
 exports.handler = function(event, context, callback) {
     callback(null, {
     statusCode: 200,
-    body: "<h1> omg, wtf</h1>"
+    body
     });
 }
