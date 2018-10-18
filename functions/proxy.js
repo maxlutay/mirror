fdf40e821463665ae33576ed7004c6b090164cae
MaximLutay <maksym.lutai@nure.ua>
2018-10-19 02:27:56 +0300

    +

--- functions/proxy.js
+++ functions/proxy.js
@@ -12,9 +12,9 @@ exports.handler = function(event, context, callback) {
     let body = "" + JSON.stringify(event) + JSON.stringify(context);
     
     if(!!source){
-        body = https.get(source).body;
+        body += https.get(source).body;
     }else {
-        body = "<h1> omg, wtf</h1>";
+        body += "<h1> omg, wtf</h1>";
     }
     
 
