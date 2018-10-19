2a4bb109b4ecd4cd857d25a3aeeb78c5a326989b
MaximLutay <maksym.lutai@nure.ua>
2018-10-20 00:53:13 +0300

    dot escape

--- functions/proxy.js
+++ functions/proxy.js
@@ -9,7 +9,7 @@ const source = process.env.SITE
 
 exports.handler = function (event, context, callback) {
     
-    const path = event.path.replace(new RegExp(".netlify/functions/proxy/","g"),"");
+    const path = event.path.replace(new RegExp("\.netlify/functions/proxy/","g"),"");
     
     let to = !path ? source : source + path;
     let body = `${to} <br> ${JSON.stringify(event)} <br> ${JSON.stringify(context)} <hr>`;
