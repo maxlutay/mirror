f3c177c28a4b7165e59414d7f3d42194b4a6b8d0
MaximLutay <maksym.lutai@nure.ua>
2018-10-22 23:28:00 +0300

    fn

--- functions/proxy.js
+++ functions/proxy.js
@@ -12,7 +12,7 @@ exports.handler = function (event, context, callback) {
     const path = event.path.replace(new RegExp("\.netlify/functions/proxy","g"),"");
     
     let to = !path ? source : source + path;
-    let body = `${to} <br> ${JSON.stringify(event)} <br> ${JSON.stringify(context)} <hr>`;
+    let body = `${path} ${source} ${to} <br> ${JSON.stringify(event)} <br> ${JSON.stringify(context)} <hr>`;
 
 
     if (!!source) {
@@ -32,7 +32,7 @@ exports.handler = function (event, context, callback) {
             statusCode: 200,
             body
         });
-    }
+    };
 
 
 
