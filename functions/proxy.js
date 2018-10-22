09cf54424180c9980186c6d1b48b8867532879f6
MaximLutay <maksym.lutai@nure.ua>
2018-10-23 00:19:13 +0300

    cscsacsaccs

--- functions/proxy.js
+++ functions/proxy.js
@@ -12,14 +12,14 @@ exports.handler = function (event, context, callback) {
     const path = event.path.replace(new RegExp("\./\.netlify/functions/proxy","g"),"");
     
     let to = !path ? source : source + path;
-    let body = `p ${path} s ${source} t ${to} <br> ${JSON.stringify(event)} <br> ${JSON.stringify(context)} <hr>`;
+    //let body = `p ${path} s ${source} t ${to} <br> ${JSON.stringify(event)} <br> ${JSON.stringify(context)} <hr>`;
 
 
     if (!!source) {
         https.get(to, res => {
             res.on("data", d => body += d);
             res.on("end", () => {
-                body += "<hr>";
+                //body += "<hr>";
                 callback(null, {
                     statusCode: 200,
                     body
