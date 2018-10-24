239048aec0c70a6be81088af2f45544f351f8e7b
MaximLutay <maksym.lutai@nure.ua>
2018-10-24 21:49:02 +0300

    to

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,7 +1,7 @@
 
 const https = require("https");
 
-const source = process.env.SITE
+const source = process.env.SITE;
 
 
 
@@ -11,31 +11,30 @@ exports.handler = function (event, context, callback) {
 
     const to = event.queryStringParameters.to;
 
-    let path = !to ? source : source + "/" + path;
+    let path = !to ? source : source + "/" + to;
     //let body = `p ${path} s ${source} t ${to} <br> ${JSON.stringify(event)} <br> ${JSON.stringify(context)} <hr>`;
 
     let body = "";
-    if (!!source) {
-        https.get(path, res => {
-            res.on("data", d => body += d);
-            res.on("end", () => {
-                body += `/*<!-- ${to}       ${JSON.stringify(event)} -->*/`
-                callback(null, {
-                    statusCode: 200,
-                    //headers: event.headers,
-                    body
-                });
-            });
-        });
-    } else {
+    if (!source) {
         body += "<h1> omg, wtf</h1>";
         callback(null, {
             statusCode: 500,
             //headers: event.headers,
             body
         });
+        return;
     };
 
-
+    https.get(path, res => {
+        res.on("data", d => body += d);
+        res.on("end", () => {
+            body += `/*<!-- ${to}       ${JSON.stringify(event)} -->*/`
+            callback(null, {
+                statusCode: 200,
+                //headers: event.headers,
+                body
+            });
+        });
+    });
 
 }
