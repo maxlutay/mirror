6df06ddee642728145ac236330a2e5491235828c
Maxim Lutay <maksym.lutai@gmail.com>
2021-05-20 10:45:36 +0300

    Changet group replacent because regexes updated

--- replacer.js
+++ replacer.js
@@ -1,10 +1,10 @@
 fetch("./.netlify/functions/proxy")
 .then(res => res.text())
 .then(text => {
-    const urlAttributesRegex = /(href|link|src|background)="(?!https?:\/\/)|(\.?\/?)([^"]+?)"/g;
-    const urlCssRegex = /url\((?!https?:\/\/)|(\.?\/?)([^)"]+?)|("[^)"]+")\)/g;
+    const urlAttributesRegex = /(href|link|src|background)="(?!https?:\/\/)|(\.?\.?\/?)([^"]+?)"/g;
+    const urlCssRegex = /url\((?!https?:\/\/)|(\.?\.?\/?)([^)"]+?)|(\"[^)"]+\")\)/g;
 
-    return text.replace(urlAttributesRegex, "$1=\"./.netlify/functions/proxy?to=$2\"")
+    return text.replace(urlAttributesRegex, "$1=\"./.netlify/functions/proxy?to=$3$4\"")
         .replace(urlCssRegex, "url(./netlify/functions/proxy?to=$1)")
         ;
 })
